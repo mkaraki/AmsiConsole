db7cc3f8875a7e167dd61868892580b0c9f42b09
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-12-22 15:49:23 +0900

    Add some features

--- AmsiConsole/Program.cs
+++ AmsiConsole/Program.cs
@@ -8,14 +8,73 @@ namespace AmsiConsole
     {
         static void Main(string[] args)
         {
-            string eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
-            byte[] eicar_bytes = Encoding.ASCII.GetBytes(eicar);
-
-            using (var amsi = new AmsiWrapper.AmsiWrapper("AmsiConsole"))
+            if (args.Length < 1)
             {
-                AMSI_RESULT result = amsi.ScanBuffer(eicar_bytes, "eicar.txt");
-                //Amsi.AMSI_RESULT result = amsi.ScanString(eicar, "eicar.txt");
-                Console.WriteLine("Result: {0}", result);
+                args = new string[] { "eicar" };
+            }
+
+            switch (args[0])
+            {
+                case "eicar":
+                    {
+                        string eicar_p1 = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$";
+                        string eicar_p2 = "EICAR-STANDARD-ANTIVIRUS-TEST-FILE!";
+                        string eicar_p3 = "$H+H*";
+                        byte[] eicar_bytes = Encoding.ASCII.GetBytes(eicar_p1 + eicar_p2 + eicar_p3);
+
+                        using (var amsi = new AmsiWrapper.AmsiWrapper("AmsiConsole"))
+                        {
+                            AMSI_RESULT result = amsi.ScanBuffer(eicar_bytes, "eicar.txt");
+                            Console.WriteLine("INTERNAL_EICAR_PATTERN_ASCII_BYTES: Result: {0}", result);
+                        }
+
+                        break;
+                    }
+
+                case "eicar_string":
+                    {
+                        string eicar_p1 = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$";
+                        string eicar_p2 = "EICAR-STANDARD-ANTIVIRUS-TEST-FILE!";
+                        string eicar_p3 = "$H+H*";
+
+                        using (var amsi = new AmsiWrapper.AmsiWrapper("AmsiConsole"))
+                        {
+                            AMSI_RESULT result = amsi.ScanString(eicar_p1 + eicar_p2 + eicar_p3, "eicar.txt");
+                            Console.WriteLine("Result: {0}", result);
+                        }
+                        break;
+                    }
+
+                case "url":
+                    {
+                        if (args.Length != 2)
+                        {
+                            Console.WriteLine("This will scan URL");
+                            Console.WriteLine("Note: This will use Invoke-WebRequest commandlet in PowerShell and scan as script. Some anti-malware solution won't scan this.");
+                            Console.WriteLine("Usage: url <url>");
+                            break;
+                        }
+
+                        string url = args[1];
+                        string urlEncode = Uri.EscapeUriString(url);
+
+                        string scanString = "Invoke-WebRequest '" + urlEncode + "'";
+
+                        using (var amsi = new AmsiWrapper.AmsiWrapper("AmsiConsole"))
+                        {
+                            AMSI_RESULT result = amsi.ScanString(scanString, url);
+                            Console.WriteLine("Result: {0}", result);
+                        }
+
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("No such commands.");
+                        Console.WriteLine("Available commands: eicar, eicar_string, url");
+                        break;
+                    }
             }
         }
     }
